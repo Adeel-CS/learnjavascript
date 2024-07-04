442ceb6206775875c346024003099ba40c3b2afe
Adeel-CS <135587537+Adeel-CS@users.noreply.github.com>
2024-07-04 10:58:14 +0500

    Read the brief.txt

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-function a(){
-    console.log(b);
+
+function parents(){
+    console.log("I'm The Parent")
+    function childern(){
+        console.log("I'm the child")
+    }
+    
 }
 
-var b =10;
-a();
+var grandparent = 8
+
+parents()
+childern()
+
+
+
+// function a(){
+//     console.log(b);
+// }
+
+// function somealert(){
+//     alert("Now jsut messing with this thing")
+// }
+
+// document.getElementById("myButton").addEventListener("click",somealert)
+
+// var b =10;
+// a();
