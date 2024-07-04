72cd537aacff7a2312fe5070bc4df1bf620f12bf
Adeel-CS <135587537+Adeel-CS@users.noreply.github.com>
2024-07-04 10:26:07 +0500

    Now it's just the beining of the learning process and heave just learned how to add script by using script tag and giving the path of the .....you know what rather than reading the writing this ass long commit i should be writing all the learned stufff in briefing,,,,,,,, ok then read the brief.txt

---
+++ index.js
@@ -0,0 +1,7 @@
+// Function to display an alert when the button is clicked
+function showAlert() {
+    alert("Button clicked!");
+}
+
+// Add an event listener to the button to call showAlert when clicked
+document.getElementById("myButton").addEventListener("click", showAlert);
