15c9981320701ff32931cc33e914a1f6725e3220
Adeel-CS <135587537+Adeel-CS@users.noreply.github.com>
2024-07-04 10:44:06 +0500

    Read the brief.txt

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// Function to display an alert when the button is clicked
-function showAlert() {
-    alert("Button clicked!");
+function a(){
+    console.log(b);
 }
 
-// Add an event listener to the button to call showAlert when clicked
-document.getElementById("myButton").addEventListener("click", showAlert);
+var b =10;
+a();
