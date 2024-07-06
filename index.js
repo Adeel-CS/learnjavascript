8f084b6d863634fda1db3dae40b289e0fbe993b7
Adeel-CS <135587537+Adeel-CS@users.noreply.github.com>
2024-07-06 12:01:40 +0500

    Callback fucntions in Javascript and how fucntions sneding ability provide us with the asynchronous programming ability

--- index.js
+++ index.js
@@ -1,65 +1,21 @@
-//First class function
+//callback functions
 
-//we can pass a fucntion inside an argument to function and even return a function and that is valid
+setTimeout(function(){
+    console.log("time out")
+}, 5000)
 
-var temp = function(parameter1){
-    console.log(parameter1)
+function x(y){
+    console.log("x")
+    y()
 }
-
-temp(function(){
-    console.log("Hi there weird programming language, you are like the game that someone has played and completed alll the stages and misssion and now every thing is unlocked and now the game is not fun anymore")
+x(function y(){
+    console.log("y")
 })
 
 
-
-// function Checking(){
-//     var check = 8, notused = 3
-//     return function temp(){
-//         console.log(check)
-//     }
-//     temp()
-// }
-
-// var newt = Checking()
-// newt()
-
-  
-  
-//     function Counter(){
-
-//         var coutner =0
-//         this.incrementing =function (){
-//             coutner++;
-//             console.log(coutner)
-//     }
-//         this.decrement = function (){
-//             coutner--;
-//             console.log(coutner)
-//         }
-// }
-
-// var counter1 = new Counter()//we have to call new since there is a constructor inside it
-
-// counter1.incrementing()
-// counter1.decrement()
-
-
-
-
-
-
-// function outer(){
-//     var a =-34;
-//     function inner(){
-//         console.log(a)
-//     }
-//     return inner
-// }
-
-// outer()()
-// //or we ca nwrite in a way
-// var close = outer()
-// close()
+//this gives the concept of not blocking the main thread when writing the JS code
+//since javascript is synchronous and runs one code line at a time therefore blocking the main thread 
+//will cause alot of problemos
 
 
 
@@ -70,85 +26,9 @@ temp(function(){
 
 
 
-// function x(){
-//     for(var i=1; i<=5; i++){
-//     function close(i){
-//     setTimeout(function() {console.log(i)
-//     }, i*1000)
-        
-//     }
-    
-//     close(i)
-// }
-        
-
-//     console.log("Hi there looking happy today")
-// }
-
-// x()
-
-
-
-
-
-
-
-
-// function x(){
-//     var a =9
-//     function y(){
-//         console.log(a)
-//     }
-//     a =234
-//     return y
-// }
-
-// var z =x()
-
-// console.log(z)
-// z()
-
-
-// function a(){
-//     var a= 8
-//     function childern(){
-//         console.log(a)
-//     }
-//     childern()
-// }
-
-// a()
-
-
-
-
-
-
-
-// function parents(){
-//     console.log("I'm The Parent")
-//     function childern(){
-//         console.log("I'm the child")
-//     }
-    
-// }
-
-// var grandparent = 8
-
-// parents()
-// childern()
-
-
-
-// function a(){
-//     console.log(b);
-// }
 
 function somealert(){
     alert("Now just messing with this thing")
 }
 
 document.getElementById("myButton").addEventListener("click",somealert)
-
-// var b =10;
-// a();
