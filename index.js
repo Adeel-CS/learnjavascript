76c50b471442986a6d1f9bcf295f9180504a12f4
Adeel-CS <135587537+Adeel-CS@users.noreply.github.com>
2024-07-14 12:16:50 +0500

    Promise.all() explained and brief has all but code of oly all

--- index.js
+++ index.js
@@ -1,68 +1,27 @@
-const carts = ["Wallet", "Shampu", "Hair Dyer"];
+const p1 = new Promise(function (resolve, reject){
+  setTimeout(()=>{reject("P1 Wierd", 5000)},3000)
+})
 
-function createOrder(cart) {
-  return new Promise((resolve, reject) => {
-    let Names = cart;
-    let numberOfProducts = cart.length;
-    let orderId = numberOfProducts * 23;
-    // console.log(numberOfProducts);
+const p2 = new Promise((resolve,reject)=>
+{
+  setTimeout(function(){
+    resolve("P2 Success")
+  },2000)
+})
 
-    if (numberOfProducts !== 0) {
-      resolve(orderId);
-    } else {
-      reject("Cart is empty");
-    }
-  });
+const p3 = new Promise((resolve, reject)=>
+{
+  setTimeout(() => {
+    reject("P3 Failed")
+  }, 4000);
 }
+)
 
-function proceedToPayment(orderId) {
-  return new Promise((resolve, reject) => {
-    // Simulate payment processing logic here
-    let paymentSuccessful = (orderId >=0); // Simulate successful payment condition
-    if (paymentSuccessful) {
-      resolve(true); // Resolve with a single boolean value
-    } else {
-      reject("Payment failed. No order ID provided.");
-    }
-  });
-}
 
-function showOrderSummary(paymentId) {
-  return new Promise((resolve, reject) => {
-    if (paymentId) {
-      let canUpdateWallet = true;
-      resolve(canUpdateWallet);
-    } else {
-      reject("The Payment was not completed");
-    }
-  });
-}
-
-function updateWallet(canUpdateWallet) {
-  return new Promise((resolve, reject) => {
-    if (canUpdateWallet) {
-      resolve("Your Wallet Has been Updated. Thanks for shopping! Do more!");
-    } else {
-      reject("There was an error in updating the wallet");
-    }
-  });
-}
-
-createOrder(carts)
-  .then(orderId => {
-    // console.log("Order ID:", orderId);
-    return proceedToPayment(orderId);
-  })
-  .then(paymentId => {
-    // console.log("Payment done:", paymentId);
-    return showOrderSummary(paymentId);
-  })
-  .then(canUpdateWallet => {
-    return updateWallet(canUpdateWallet);
-  })
-  .then(message => {
-    console.log(message);
-  })
-  .catch(error => {
-    console.log("Error:", error);
-  });
+Promise.all([p1, p2, p3])
+.then((res)=>{
+  console.log(res)
+})
+.catch((err)=>{
+  console.error(err)
+})
