bb1137ed4e912c762698d5445edf90365422835b
Adeel-CS <135587537+Adeel-CS@users.noreply.github.com>
2024-07-16 14:39:57 +0500

    The await keyword in the async fucntion and how it is suspended from the main thread and act as backgrounf running program

--- briefing.md
+++ briefing.md
@@ -5,3 +5,4 @@
 ** await is a keyword that ---can only--- be used inside an async fucntion**
 Now await will give the resolved value of the promise written after it!!!!!!!!!___
 ---
+#  Asyn await will not block the main thread rather the function that is calling the await in the async fucntion will be suspended till the Promise after the ** await ** is resolved--  The Call Stack Waits for non in the JavaScript Engine
--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const APromise = new Promise((resolve, reject)=>{
-  if(3=="3"){
-    return resolve("Well it Does work like that !!___")
-  }
-  else if(3===3){
-    return reject("Well It does Work Like That for sure")
-  }
+  setTimeout(()=>{
+    if(3=="3"){
+      return resolve("Well it Does work like that !!___")
+    }
+    else if(3===3){
+      return reject("Well It does Work Like That for sure")
+    }
+  },5000)
+
 })
 
+
+const APromise2 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    return resolve("The Second Promise is Resolved Now")
+  },2000)
+})
+
+
 async function checking(){
+  console.log("Temporary check this will be reieved first")
+
+  //First Pormise With Higher SetTimeout
   const val = await APromise
+  console.log("The Higher SettimeOut Will won, it will take the higher time")
   console.log(val)
-  APromise.then((res)=>{
-    console.log(res)
-  })
-  .catch((err)=>{
-    console.error(err)
-  })
+
+  //Second Promise With lesser TimeOut
+  const val2 = await APromise2
+  console.log("Chekking the Second Promise That is Of lesser Time")
+
+
+
+
+  // APromise.then((res)=>{
+  //   console.log(res)
+  // })
+  // .catch((err)=>{
+  //   console.error(err)
+  // })
 }
 
 checking()
